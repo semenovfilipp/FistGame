59492ff3132b09e72d4f50932c311a115b359396
semenovfilipp <znexiss@gmail.com>
2023-04-22 09:10:23 +0300

    second lesson - 1/2 mouse inputs

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,11 +1,16 @@
 package main;
 
+import inputs.KeyBoardInputs;
+
+import java.awt.event.KeyListener;
+
 public class Game {
     private GamePanel gamePanel;
     private GameWindow gameWindow;
     public Game(){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
+        gamePanel.requestFocus();
 
     }
 
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,13 +1,37 @@
 package main;
 
+import inputs.KeyBoardInputs;
+import inputs.MouseInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private int xDelta = 0, yDelta = 0;
+
     public GamePanel(){
+        mouseInputs = new MouseInputs();
+        addKeyListener(new KeyBoardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
+    public void changeXDelta(int value){
+        this.xDelta += value;
+        repaint();
+    }
+    public void changeYDelta(int value){
+        this.yDelta += value;
+        repaint();
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.fillRect(100,100,200,50);
+        g.fillRect(100 + xDelta,100 + yDelta,200,50);
     }
 }
